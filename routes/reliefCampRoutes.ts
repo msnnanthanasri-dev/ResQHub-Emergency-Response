5c6dfc0ee8ee02c5b9b8d5807ac368e18cfc688d
Nanthana Sri M <msnnanthanasri@gmail.com>
2026-09-06 08:59:25 +0530

    Update reliefCampRoutes.ts

--- routes/reliefCampRoutes.ts
+++ routes/reliefCampRoutes.ts
@@ -3,15 +3,15 @@ import { Router } from "express";
 import {
   getReliefCamps,
   createReliefCamp,
+  updateReliefCamp,
   deleteReliefCamp,
 } from "../controllers/reliefCampController";
 
 const router = Router();
 
 router.get("/", getReliefCamps);
-
 router.post("/", createReliefCamp);
-
+router.put("/:id", updateReliefCamp);
 router.delete("/:id", deleteReliefCamp);
 
 export default router;
