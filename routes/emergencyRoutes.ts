01c18ff50a4d96ea6414a01b79794d04836ee9ea
Nanthana Sri M <msnnanthanasri@gmail.com>
2026-09-06 08:58:58 +0530

    Update emergencyRoutes.ts

--- routes/emergencyRoutes.ts
+++ routes/emergencyRoutes.ts
@@ -1,12 +1,20 @@
 import express from "express";
+
 import {
   getEmergencyReports,
   createEmergencyReport,
+  updateEmergencyReport,
+  deleteEmergencyReport,
 } from "../controllers/emergencyController";
 
 const router = express.Router();
 
 router.get("/", getEmergencyReports);
+
 router.post("/", createEmergencyReport);
+
+router.put("/:id", updateEmergencyReport);
+
+router.delete("/:id", deleteEmergencyReport);
 
 export default router;
