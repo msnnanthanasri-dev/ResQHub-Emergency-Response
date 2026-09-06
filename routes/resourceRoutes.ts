0647b473672dabd6b0560ce1087fdeb2af9e6aba
Nanthana Sri M <msnnanthanasri@gmail.com>
2026-09-06 08:59:54 +0530

    Update resourceRoutes.ts

--- routes/resourceRoutes.ts
+++ routes/resourceRoutes.ts
@@ -3,13 +3,22 @@ import { Router } from "express";
 import {
   getResources,
   createResource,
+  updateResource,
   deleteResource,
 } from "../controllers/resourceController";
 
 const router = Router();
 
+// Get all resources
 router.get("/", getResources);
+
+// Add a new resource
 router.post("/", createResource);
+
+// Edit / update an existing resource
+router.put("/:id", updateResource);
+
+// Delete a resource
 router.delete("/:id", deleteResource);
 
 export default router;
