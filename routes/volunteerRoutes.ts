c2c737733595ecb21b0ec65a9747eb384bcd77dc
Nanthana Sri M <msnnanthanasri@gmail.com>
2026-09-06 09:00:18 +0530

    Update volunteerRoutes.ts

--- routes/volunteerRoutes.ts
+++ routes/volunteerRoutes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import {
   getVolunteers,
   createVolunteer,
+  updateVolunteer,
   deleteVolunteer,
 } from "../controllers/volunteerController";
 
@@ -11,6 +12,8 @@ const router = Router();
 router.get("/", getVolunteers);
 
 router.post("/", createVolunteer);
+
+router.put("/:id", updateVolunteer);
 
 router.delete("/:id", deleteVolunteer);
 
